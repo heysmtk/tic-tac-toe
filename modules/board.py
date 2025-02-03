2ad66f26afa369f0c69f39795ca70d77f2807f38
heysmtk <smtktom@gmail.com>
2025-02-03 20:54:22 +0100

    delete board.py

--- modules/board.py
+++
@@ -1,9 +0,0 @@
-# Creating a play board 3x3
-board = [[" " for _ in range(3)] for _ in range(3)]
-
-
-# Printing play board
-def board_print():
-    for row in board:
-        print("|".join(row))
-        print("-" * 5)
